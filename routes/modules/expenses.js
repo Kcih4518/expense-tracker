78c05dfba445921a7ee3828ed63f164e7e69b71e
Kcih4518 <kcih4518@gmail.com>
2021-07-22 21:36:45 +0800

    <feat>[AC-S2-3-W4-Expense-Tracker]: Add 'create' feature : display the form for creating expenses record
    1. Get categories from DB
    2. Use DB data to render add.hbs

--- routes/modules/expenses.js
+++ routes/modules/expenses.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const router = express.Router()
+const Category = require('../../models/categories')
 
-// Read: Sort by selection
-router.get('/', (req, res) => {
-  res.send('hello from simple server : expenses')
+// Create : Display the form for creating expenses record
+router.get('/add', (req, res) => {
+  return Category.find()
+    .lean()
+    .then((categories) => {
+      res.render('add', { categories })
+    })
+    .catch((error) => console.log(error))
 })
 
 module.exports = router
