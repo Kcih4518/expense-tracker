64e3384894ffe391c7bc46fd068d1f80d37d83da
Kcih4518 <kcih4518@gmail.com>
2021-07-22 21:03:49 +0800

    <feat>[AC-S2-3-W4-Expense-Tracker]: Add 'read' feature to render all records、categories and totalAmount
    1. Use mongoose find() to get all categories & records
    2. totalAmount += record.amount
    3. Pass all data to the index.hbs

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,9 +1,29 @@
 const express = require('express')
 const router = express.Router()
+const Record = require('../../models/records')
+const Category = require('../../models/categories')
 
 // Read : View all the expenses
-router.get('/', (req, res) => {
-  res.send('hello from simple server : home)')
+router.get('/', async (req, res) => {
+  let totalAmount = 0
+  const categories = await Category.find().lean()
+  return Record.find()
+    .lean()
+    .sort({ date: 'asc' })
+    .then((records) => {
+      records.forEach((record) => {
+        totalAmount += record.amount
+      })
+
+      res.render('index', {
+        records,
+        categories,
+        totalAmount
+      })
+    })
+    .catch((error) => {
+      console.log(error)
+    })
 })
 
 module.exports = router
