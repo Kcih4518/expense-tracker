c4c2c62eecd56adabb1d36b154070f123e5978a7
Kcih4518 <kcih4518@gmail.com>
2021-07-23 13:31:26 +0800

    <feat>[AC-S2-3-W4-Expense-Tracker]: Add 'delete' feature to remove expenses record from DB data
    1. href='./expenses/{{this._id}}?_method=DELETE'
    2. Use req.params to get id
    3. Get record by mongoose findById
    4. Remove record from mongoDB
    5. When the Remove is successful, redirect the page to /

--- routes/modules/expenses.js
+++ routes/modules/expenses.js
@@ -52,4 +52,14 @@ router.put('/:id', (req, res) => {
     .catch((error) => console.log(error))
 })
 
+// Delete : Remove expenses record
+// TODO: Error handle : When cannot delete DB data
+router.delete('/:id', (req, res) => {
+  const id = req.params.id
+  Record.findById(id)
+    .then((record) => record.remove())
+    .then(() => res.redirect('/'))
+    .catch((error) => console.log(error))
+})
+
 module.exports = router
