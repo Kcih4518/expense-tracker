f97cd72d9382d9fa72249a18c2e5e359672ddfb9
Kcih4518 <kcih4518@gmail.com>
2021-08-10 15:40:42 +0800

    <feat>[AC-S2-3-W4-Expense-Tracker]: Add user model to define user schema
    1. name、email、password、createdAt

---
+++ models/users.js
@@ -0,0 +1,26 @@
+// Require node_modules
+const mongoose = require('mongoose')
+
+// Define user schema
+const Schema = mongoose.Schema
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+// Export module
+module.exports = mongoose.model('User', userSchema)
