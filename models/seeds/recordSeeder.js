fb5e27fed08d38b39bbb06d13d24a1e7cf0ce678
Kcih4518 <kcih4518@gmail.com>
2021-08-10 20:18:06 +0800

    <refactor>[AC-S2-3-W4-Expense-Tracker]: Modify recordSeeder.js to create records with userID

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,16 +1,43 @@
 // Require node_modules
 const Record = require('../records')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const db = require('../../config/mongoose')
 const recordsJson = require('./records.json')
+const User = require('../users')
+const bcrypt = require('bcryptjs')
+
+const SEED_USER = {
+  name: 'root',
+  email: 'root@example.com',
+  password: '12345678'
+}
 
 // Setting mongoose
 db.once('open', () => {
-  Record.create(recordsJson)
+  bcrypt
+    .genSalt(10)
+    .then((salt) => bcrypt.hash(SEED_USER.password, salt))
+    .then((hash) =>
+      User.create({
+        name: SEED_USER.name,
+        email: SEED_USER.email,
+        password: hash
+      })
+    )
+    .then((user) => {
+      const userID = user._id
+      recordsJson.forEach((record) => (record.userID = userID))
+      return Promise.all(
+        Array.from({ length: recordsJson.length }, (_, i) =>
+          Record.create(recordsJson[i])
+        )
+      )
+    })
     .then(() => {
-      db.close()
-      console.log('Import record data into database successfully!')
+      console.log('done')
+      process.exit()
     })
-    .catch((error) => {
-      console.log(error)
-    })
+    .catch((err) => console.log(err))
 })
