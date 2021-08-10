c0cb9605b2c9004ef0df67442c8fb68acb9e1dc9
Kcih4518 <kcih4518@gmail.com>
2021-08-10 18:55:34 +0800

    <fix>[AC-S2-3-W4-Expense-Tracker]: Fix config/passport.js cannot find user model

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,7 @@
 // Require node_modules
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
-const User = require('../models/user')
+const User = require('../models/users')
 const bcrypt = require('bcryptjs')
 
 module.exports = (app) => {
