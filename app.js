63793a766f26de50c1c1e28301a786030fc9d001
Kcih4518 <kcih4518@gmail.com>
2021-08-10 19:17:18 +0800

    <feat>[AC-S2-3-W4-Expense-Tracker]: Set login status store in res.locals

--- app.js
+++ app.js
@@ -54,6 +54,8 @@ app.use(flash())
 
 // Setting middleware to store info into res.locals
 app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
   res.locals.success_msg = req.flash('success_msg')
   res.locals.warning_msg = req.flash('warning_msg')
   next()
