f7df0ea9b3b94b5ad61c1d36b8201a764c0759f9
Kcih4518 <kcih4518@gmail.com>
2021-08-10 15:50:53 +0800

    <feat>[AC-S2-3-W4-Expense-Tracker]: Modify index.js to import users module

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const router = express.Router()
 const home = require('./modules/home')
 const expenses = require('./modules/expenses')
+const users = require('./modules/users')
 
-router.use('/', home)
 router.use('/expenses', expenses)
+router.use('/users', users)
+router.use('/', home)
 
 module.exports = router
